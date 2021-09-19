45e59831ccf59a914d634c88664aeb88c82bf99c
Eldar Zainashev <aeoloth@yandex.ru>
2021-09-19 10:40:22 +0300

    234

--- hw-6-t8/src/App.js
+++ hw-6-t8/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+
 
 
 export default class App extends Component {
@@ -22,27 +22,11 @@ export default class App extends Component {
   render() {
     console.log('render')
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>STAR GATE</Text>
-    </View>
+    <div className="viewStyle">
+      <label className="textStyle">STAR GATE</label>
+    </div>
   );
   }
 }
 
-const styles = StyleSheet.create({
-  viewStyle: {
-    backgroundColor: '#30d0fe',
-    height: 116,
-    justifyContent: 'center',
-    paddingLeft: 22,
-    paddingTop: 71,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    elevation: 2,
-    position: 'relative'
-  },
-  textStyle: {
-    justifyContent: 'center'
-  }
-})
+
